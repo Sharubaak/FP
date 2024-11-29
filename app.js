a766df47e8e64ce5aa023861d4e96f857b39c40b
Sharubaa <146009109+Sharubaa@users.noreply.github.com>
2024-11-29 00:57:27 -0500

    Quick fix

--- app.js
+++ app.js
@@ -1,46 +1,40 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let session = require('express-session');
-let passport = require('passport');
-let passportLocal = require('passport-local');
-let localStrategy = passportLocal.Strategy;
-let flash = require('connect-flash');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('passport-local');
+const localStrategy = passportLocal.Strategy;
+const flash = require('connect-flash');
+const createError = require('http-errors');
 
 // Initialize Express
-let app = express(); // Move this line to the top before using `app`
+const app = express();
 
-// Connect to MongoDB (add your connection code here)
 
-// Set up Express session
+// Session Configuration
 app.use(session({
   secret: "OurSecret",
   saveUninitialized: false,
   resave: false
 }));
 
-// Initialize flash
+// Flash Messages
 app.use(flash());
 
-// Initialize Passport
+// Passport Initialization
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Create a user model instance
-let userModel = require('./models/user');
-let user = userModel.User;
-
-// Serialize and deserialize the user information
+// User Model
+const userModel = require('./models/user');
+const user = userModel.User;
+passport.use(new localStrategy(user.authenticate()));
 passport.serializeUser(user.serializeUser());
 passport.deserializeUser(user.deserializeUser());
 
-// Routers
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-
-// View engine setup
+// Set View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -50,24 +44,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
 
-// Use routers
+// Pass Flash Messages to Views
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+// Catch 404 and Forward to Error Handler
+app.use((req, res, next) => {
+  res.status(404).render('404', { title: 'Page Not Found' });
 });
 
-// Error handler
-app.use(function (err, req, res, next) {
-  // Set locals, only providing error in development
+// Error Handler
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
