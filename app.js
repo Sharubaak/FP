9fce828a6d9f1c7ca00d63a09c05def7475165c6
Sharubaa <146009109+Sharubaa@users.noreply.github.com>
2024-11-29 20:09:57 -0500

    Quick fix

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -13,7 +12,6 @@ const createError = require('http-errors');
 // Initialize Express
 const app = express();
 
-
 // Session Configuration
 app.use(session({
   secret: "OurSecret",
@@ -21,7 +19,7 @@ app.use(session({
   resave: false
 }));
 
-// Flash Messages
+// Flash Messages Middleware
 app.use(flash());
 
 // Passport Initialization
@@ -36,36 +34,18 @@ passport.serializeUser(user.serializeUser());
 passport.deserializeUser(user.deserializeUser());
 
 // Set View Engine
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
-
-// Middleware
-=======
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const surveyRouter = require('./routes/survey');
-
-var app = express();
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
->>>>>>> 359defea52fb82faba939b220ad5de9b09e4f084
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-<<<<<<< HEAD
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
-// Pass Flash Messages to Views
+// Flash Messages to Views
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
@@ -76,44 +56,24 @@ app.use((req, res, next) => {
 // Routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// Catch 404 and Forward to Error Handler
-app.use((req, res, next) => {
-  res.status(404).render('404', { title: 'Page Not Found' });
-});
-
-// Error Handler
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-=======
-app.use(express.static(path.join(__dirname, 'node_modules')));
+const surveyRouter = require('./routes/survey');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/survey', surveyRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Catch 404 and Render Custom 404 Page
+app.use((req, res, next) => {
+  res.status(404).render('404', { title: 'Page Not Found' });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Global Error Handler
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
->>>>>>> 359defea52fb82faba939b220ad5de9b09e4f084
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: 'Error' });
 });
 
 module.exports = app;
-<<<<<<< HEAD
-=======
-  
->>>>>>> 359defea52fb82faba939b220ad5de9b09e4f084
+
