8f398e0074d4342ef19cbbd1a529d84a82da476a
kezzykezzy <kezia.bello@ontariotechu.net>
2024-12-05 23:22:23 -0500

    editing

--- db.js
+++ db.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-module.exports = {URI: "mongodb+srv://carolinenkan:GPzHyuPJW2fvWXPv@cluster1.72dms.mongodb.net/"}; //online database
-//mongodb+srv://carolinenkan:GPzHyuPJW2fvWXPv@cluster1.72dms.mongodb.net//mongo compass database
-=======
 module.exports = {"URI":"mongodb+srv://carolinenkan:GPzHyuPJW2fvWXPv@cluster1.72dms.mongodb.net/"}
->>>>>>> 1b36061666a9fa579c887b2296ba06a464c39e29
--- routes/survey.js
+++ routes/survey.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const router = express.Router();
-const Survey = require('../models/Survey');
 
 // Placeholder for survey entries
 let surveys = [];
